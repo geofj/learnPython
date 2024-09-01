af3aa7c2fafe97b5956bd7f4995a2773c203fa05
geofj <joeyg.dev@proton.me>
2024-09-01 14:12:04 -0400

    new: created type function to determine type of variable

---
+++ app1/Section2/coding excercises/ce1.py
@@ -0,0 +1,11 @@
+# Write a Python program following the steps below:
+#
+# 1. Create a string and associate it with a variable named my_variable.
+#
+# 2. Print out the type of the variable.
+
+my_variable = "This is a string"
+# my_variable stores the string
+
+print(type(my_variable))
+# print function along with the type function now displays the type for my_variable
