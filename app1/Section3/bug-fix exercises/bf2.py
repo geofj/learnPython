f96e70d809e1152b32ffc284b06dd892bc0aec91
geofj <joeyg.dev@proton.me>
2024-09-01 17:26:35 -0400

    fix: reformatted print function so upper is called correctly

---
+++ app1/Section3/bug-fix exercises/bf2.py
@@ -0,0 +1,11 @@
+# The programmer here is trying to convert the string "hello" to "HELLO" by using the upper() method:
+# code below:
+
+# greeting = "hello"
+# print(upper(greeting))
+
+# However, the program returns an error. Can you help fix the code, so it prints out HELLO?
+
+greeting = "hello"
+print(greeting.upper())
+# added greeting variable before the upper function, so the upper function is correctly called
