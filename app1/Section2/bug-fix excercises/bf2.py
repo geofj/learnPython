e0d6e77852ddce3504c8149b0db5500c698b3c03
geofj <joeyg.dev@proton.me>
2024-09-01 15:09:26 -0400

    fix: added missing commas between string and variable

---
+++ app1/Section2/bug-fix excercises/bf2.py
@@ -0,0 +1,9 @@
+# The following code throws an error. Try to fix it.
+# code below:
+
+# my_answer = input("What is your answer?")
+# answers = ['Yes', 'No', 'Yes' 'No' my_answer]
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+# answers list was missing ","
