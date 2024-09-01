e0342d2f687e065a67525664897ec58648d2754c
geofj <joeyg.dev@proton.me>
2024-09-01 17:23:21 -0400

    fix: added colon to while loop and indentation for print statement

---
+++ app1/Section3/bug-fix exercises/bf1.py
@@ -0,0 +1,10 @@
+# The code below has two bugs. Hunt them down and fix them.
+# code below:
+
+# while True
+# print("Hello")
+
+while True:
+# added colon to complete while loop
+    print("Hello")
+    # added indentation to fix the while loop
