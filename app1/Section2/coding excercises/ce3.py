acfa5e269e11816d28e71ef6159048236fc7a33e
geofj <joeyg.dev@proton.me>
2024-09-01 14:21:32 -0400

    new: created a list with 4 strings

---
+++ app1/Section2/coding excercises/ce3.py
@@ -0,0 +1,7 @@
+# Create a list of four strings and associate the list with a variable named my_variable.
+
+my_variable = ["sunny", "cloudy", "rainy", "snowy"]
+# a list with four strings associated to my_variable variable
+
+print(my_variable)
+# prints the strings within my_variable
