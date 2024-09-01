9077449bae7e4fc9f684fba87ca9b403e8923a42
geofj <joeyg.dev@proton.me>
2024-09-01 15:58:01 -0400

    new: program to print assigned variables

---
+++ app1/Section3/coding excercises/ce1.py
@@ -0,0 +1,12 @@
+# Assign some values to variables x, y, and z.
+# Then, insert x, y, and z inside a print() function to print out the values of those variables.
+
+x = 7
+# value for x is the integer 7
+y = 2
+# value for y is the integer 2
+z = 14
+# value for z is the integer 14
+
+print(x, y, z)
+# prints the values for variables x, y, and z
