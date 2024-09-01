187914c020680e4574e6bae8028deefda107696e
geofj <joeyg.dev@proton.me>
2024-09-01 16:01:40 -0400

    new: program to print assigned variables

---
+++ app1/Section3/coding excercises/ce2.py
@@ -0,0 +1,7 @@
+# Create a program that prompts the user to input their name once.
+# Then, the program prints out the name once with the first letter capitalized.
+
+user_name = input("Please enter your name: ")
+# variable that stores the user name input
+print(user_name.capitalize())
+# prints the stored user name input
