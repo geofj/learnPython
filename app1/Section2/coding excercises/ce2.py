06955b7cd779488fe0a27f97be285ba1857bb622
geofj <joeyg.dev@proton.me>
2024-09-01 14:16:39 -0400

    new: created type function to collect user input and display it

---
+++ app1/Section2/coding excercises/ce2.py
@@ -0,0 +1,7 @@
+# Write a Python program that gets input from the user and prints out that input.
+
+user_input = input("Please enter your name: ")
+# input function that asks user for their name
+
+print(user_input)
+# prints input from the user_input variable
