1c2d2b064d745fd3ce6d7be5cf30402ffe3cbfa2
geofj <joeyg.dev@proton.me>
2024-09-01 13:54:27 -0400

    feat: added list function & individual todo user inputs to build tolist

---
+++ app1/Section2/main-2.py
@@ -0,0 +1,17 @@
+# Storing user inputs
+
+user_prompt = "Enter a todo: "
+# variable user_prompt holds the string
+todo1 = input(user_prompt)
+# variable todo1 calls the user_prompt string
+todo2 = input(user_prompt)
+# variable todo2 calls the user_prompt string
+todo3 = input(user_prompt)
+# variable todo3 calls the user_prompt string
+
+todos = [todo1, todo2, todo3]
+# a list that holds all 3 user inputs
+
+print(todos)
+# print function that prints the input from todos variable
+
