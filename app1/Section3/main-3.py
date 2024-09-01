f0ea7ecf47062e082c75a04576c1367919d6631e
geofj <joeyg.dev@proton.me>
2024-09-01 15:22:53 -0400

    feat: added a while-loop to continuously grab user input

---
+++ app1/Section3/main-3.py
@@ -0,0 +1,14 @@
+# Getting user input in a batch operation using while-loop
+
+user_prompt = "Enter a todo: "
+# variable user_prompt holds the string
+
+while True:
+# creates a loop operation based on the True boolean
+    todo = input(user_prompt)
+    # variable todo1 calls the user_prompt string
+    print(todo)
+    # print function that prints the input from todos variable
+    print("Next...")
+    # prints cue to type in the next todos
+
