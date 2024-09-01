c2ba3f3802de5af575d150fcd1fa7b764c9021ec
geofj <joeyg.dev@proton.me>
2024-09-01 15:11:47 -0400

    fix: added missing double quotes for input string

---
+++ app1/Section2/bug-fix excercises/bf3.py
@@ -0,0 +1,9 @@
+# The following code has an error. Try to fix it.
+# code below:
+
+#my_answer = input(What is your answer?)
+#answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+# missing quotes for strings in the input function
