a968215f84fc7e9d6db59796fcfd61e52a79b1ac
geofj <joeyg.dev@proton.me>
2024-09-01 18:05:08 -0400

    new: added match case to guide user interaction

---
+++ app1/Section4/main-5.py
@@ -0,0 +1,21 @@
+# Create a list view of the todos and have an exit option for program using match-case
+
+todos = []
+# empty list to store todos to be printed
+
+while True:
+# creates a loop operation based on the True boolean
+    start_action = input("Would you like to add or show list: ")
+    # user input of 'add' or 'show' to guide the user into a match case for either options
+
+    match start_action:
+        case 'add':
+        # first case 'add' results in user entering to do item and appending the item to todos list
+            todo = input("Enter a todo: ")
+            # input variable calls the user_prompt string
+            todos.append(todo)
+            # append function appends the input into the empty todos list
+        case 'show':
+        # second case will show the newly appended list
+            print(todos)
+            # prints the newly appended todos list
