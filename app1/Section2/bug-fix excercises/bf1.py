aa3a6b4f6666854d834a2834d6e612a2570425bd
geofj <joeyg.dev@proton.me>
2024-09-01 15:06:18 -0400

    fix: added ] to close the list

---
+++ app1/Section2/bug-fix excercises/bf1.py
@@ -0,0 +1,6 @@
+# The following code has a syntax error. Try to fix it.
+# code below:
+# answers = ['Yes', 'No', 'Yes', 'No', 'No'
+
+answers = ['Yes', 'No', 'Yes', 'No', 'No']
+# the list was missing "]" at the end
