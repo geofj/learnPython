0f39ff914fa4f9836756fe7ac578b5641f87d833
geofj <joeyg.dev@proton.me>
2024-09-01 23:34:01 -0400

    new: tuples

---
+++ app1/Section5/coding exercises/ce5.py
@@ -0,0 +1,8 @@
+# Create a color_codes variable and assign a tuple to it.
+# The tuple should contain three tuples as items.
+# Each of the three tuples should contain three items. The items can be any type.
+
+color_codes = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
+# color_codes variable holds a 3 tuples inside a single tuple
+print(color_codes)
+# print to confirm number of tuples
