cdf31dec5a00c830f112c9d4970e7d5b591ebddd
geofj <joeyg.dev@proton.me>
2024-09-01 23:30:44 -0400

    new: values and value types

---
+++ app1/Section5/coding exercises/ce4.py
@@ -0,0 +1,16 @@
+# Create three variables mood, strength, and rank and assign a string to mood, a float to strength, and an integer to rank.
+# The values you assign can be anything as long as they are of the correct type.
+
+mood = "happy"
+# mood variable is assigned a string
+strength = 8.7
+# strength variable is assigned a float
+rank = 2
+# rank variable is assigned an integer
+
+print(type(mood))
+print(type(strength))
+print(type(rank))
+# print functions to confirm value types
+
+
