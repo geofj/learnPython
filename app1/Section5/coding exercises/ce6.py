a818925e4ef8a48f169eda9021b2613bc1febea6
geofj <joeyg.dev@proton.me>
2024-09-01 23:37:30 -0400

    new: index list

---
+++ app1/Section5/coding exercises/ce6.py
@@ -0,0 +1,6 @@
+# Complete the script so that it prints out the 3rd item of list serials.
+
+serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+# serials variable holds the index list of serial numbers
+print(serials[2])
+# prints the serial number in 3rd place, but index rank of 2
