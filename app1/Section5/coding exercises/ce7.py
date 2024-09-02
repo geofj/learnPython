fbdc6e3ca3084ecb537580fc35e819cbdf21fdb8
geofj <joeyg.dev@proton.me>
2024-09-01 23:42:21 -0400

    new: appending the index list

---
+++ app1/Section5/coding exercises/ce7.py
@@ -0,0 +1,11 @@
+# Append the value of current to the end of the list seconds.
+# Please use the list.append() method to accomplish that.
+
+seconds = [1.23, 1.45, 1.02]
+# seconds variable holds the list of seconds
+current = 1.11
+# the current seconds time
+seconds.append(current)
+# appends the current time to the list of seconds
+print(seconds)
+# print the new list of seconds with the appended item
