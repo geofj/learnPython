0b145c6ef93c163c1be9049638d0157e3df2743b
geofj <joeyg.dev@proton.me>
2024-09-01 20:29:23 -0400

    new: deploying a for-loop to print out integers

---
+++ app1/Section4/coding exercise/ce3.py
@@ -0,0 +1,16 @@
+# Loop over the colors items and print out the item in every loop. So, the expected output of your code would be:
+#
+# 11
+# 34
+# 98
+# 43
+# 45
+# 54
+# 54
+
+colors = [11, 34, 98, 43, 45, 54, 54]
+# colors variable stores the integers
+
+for color in colors:
+    print(color)
+    # for loop prints each integer from the list of colors
