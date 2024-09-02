6fd4a06112f982f8e9f8f5d2fa5fe8a6a5dd4512
geofj <joeyg.dev@proton.me>
2024-09-01 20:35:07 -0400

    fix: indentation for print function

---
+++ app1/Section4/bug-fix excercises/bf2.py
@@ -0,0 +1,13 @@
+# The programmer is again missing something in the code. Try to find what it is and fix it.
+# code below:
+
+# buttons = ["cancel", "reply", "submit"]
+#
+# for i in buttons:
+# print(i.capitalize())
+
+buttons = ["cancel", "reply", "submit"]
+
+for i in buttons:
+    print(i.capitalize())
+    # print function was not indented properly, added indentation so code executes
