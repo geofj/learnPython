c3bf4064ac0be5240bfc0b779659046904591aa0
geofj <joeyg.dev@proton.me>
2024-09-01 23:45:27 -0400

    fix: changed () to [] to properly call index rank form list

---
+++ app1/Section5/bug-fix exercises/bf1.py
@@ -0,0 +1,10 @@
+# The programmer is trying to extract and print out 'b' using list indexing.
+# But there is an error. Try to fix it.
+# code below:
+
+# elements = ['a', 'b', 'c']
+# print(elements(1))
+
+elements = ['a', 'b', 'c']
+print(elements[1])
+# changed (1) to [1] to properly call the right index from list
