9455662f34d8f5b66c75e0af5f4c2faec6a6c601
geofj <joeyg.dev@proton.me>
2024-09-03 12:10:43 -0400

    new: remove an item from the list using remove method

---
+++ app1/Section6/coding exercises/ce5.py
@@ -0,0 +1,8 @@
+# Remove item 1.45 from seconds.
+
+seconds = [1.23, 1.45, 1.02, 1.11]
+
+remove = seconds.remove(1.45)
+# remove method removes the item within the arg
+print(seconds)
+# prints the updated list
