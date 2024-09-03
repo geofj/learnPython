b663458fc1d7a7fc9efe5cedb1a62b521a7a262f
geofj <joeyg.dev@proton.me>
2024-09-03 12:07:40 -0400

    new: list containing a float, integer, string and another list

---
+++ app1/Section6/coding exercises/ce4.py
@@ -0,0 +1,6 @@
+# Assign a list to the rainfall variable.
+# The list should contain four items, a float, an integer, a string, and a list*.
+# *It's totally fine for a list to contain another list as an item.
+
+rainfall = [3.2, 5, "medium", [1, 2.2, "tall"]]
+# list containing a float, integer, string and another list
