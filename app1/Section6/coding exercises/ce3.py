2f0ca6c721d1a9277bd6186ca608dbb2a5035d59
geofj <joeyg.dev@proton.me>
2024-09-03 12:04:52 -0400

    new: list containing a float, integer and a string

---
+++ app1/Section6/coding exercises/ce3.py
@@ -0,0 +1,5 @@
+# Assign a list to the variable temperatures.
+# The list should contain three items, a float, an integer, and a string.
+
+temperatures = [78.2, 80, "sunny"]
+# list containing a float, integer and string
